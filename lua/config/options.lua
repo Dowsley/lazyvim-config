4f839a0f0de8d573351d6d488d07b0867b0e9ff5
Dowsley <joao.dowsley850@gmail.com>
2026-03-20 18:12:58 -0700

    more responsive

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,6 @@
 -- Add any additional options here
 
 vim.opt.guicursor = "a:block"
+vim.opt.mousescroll = "ver:3,hor:0"
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 300
---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      max_lines = 5,
+      separator = nil,
+      mode = "topline",
+    },
+  },
+}
