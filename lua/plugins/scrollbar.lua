1549c7c2be2b2318927843ed3b9c126650cb140f
Dowsley <joao.dowsley850@gmail.com>
2026-03-21 16:14:31 -0700

    scrollbar

---
+++ lua/plugins/scrollbar.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "dstein64/nvim-scrollview",
+    dependencies = { "lewis6991/gitsigns.nvim" },
+    event = "BufReadPost",
+    init = function()
+      vim.g.scrollview_diagnostics_error_symbol = "▪"
+      vim.g.scrollview_diagnostics_warn_symbol = "▪"
+      vim.g.scrollview_diagnostics_info_symbol = "▪"
+      vim.g.scrollview_diagnostics_hint_symbol = "▪"
+    end,
+    config = function()
+      require("scrollview").setup({
+        signs_on_startup = {
+          "diagnostics",
+          "search",
+          "quickfix",
+        },
+        mousemoveevent = true,
+      })
+      require("scrollview.contrib.gitsigns").setup()
+    end,
+  },
+}
